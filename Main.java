86a9916be26cc4c2f89a235f4f82bd84dde7ac36
yashsingh-glitch <pratapyashsingh21@gmail.com>
2025-11-13 18:47:54 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,46 @@
+//Given an array arr[] containing only non-negative integers, your task is to find a continuous
+// subarray (a contiguous sequence of elements) whose sum equals a specified value target.
+// You need to return the 1-based indices of the leftmost and rightmost elements of this subarray.
+// You need to find the first subarray whose sum is equal to the target.
 
-//Condition-arrays elements are always in a sorted manner
-//Given an sorted array arr[] of integers. Sort the array into a wave-like array(In Place). In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5] ..... and so on. If there are multiple solutions, find the lexicographically smallest one.
-//
-//Note: The given array is sorted in ascending order, and modify the given array in-place without returning a new array.
-
+      //  Note: If no such array is possible then, return [-1].
+import java.util.*;
 class Solution {
-    public void swap(int arr[] , int i,int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j]=temp;
-    }
-    public void sortInWave(int arr[]) {
+    static void subarraySum(int[] arr, int target) {
         // code here
-        int i =0;
+        ArrayList<Integer> res = new ArrayList<>();
+        int i = 0;
+        int j =0;
         while(i<arr.length-1){
-            int j = i+1;
-            swap(arr,i,j);
-            i=i+2;
+            int sum = 0;
+            for(j = i;j<arr.length;j++){
+
+                if(target-sum>=arr[j]){
+                    sum=sum+arr[j];
+                }
+                else{
+                    break;
+                }
+            }
+            if(target - sum== 0){
+                res.add(i+1);
+                res.add(j);
+                break;
+            }
+            i++;
+        }
+        if(res.isEmpty()){
+            res.add(-1);
         }
 
     }
 }
+
 public class Main {
 
     public static void main(String[] args) {
         int[] arr = {2,4,5,6,7};
         Solution S = new Solution();
-        S.sortInWave(arr);
-        for(int i : arr){
-            System.out.println(i);
-        }
+        S.subarraySum(arr,9);
         }
     }
